ac07276cc08e0bd7a0fa510e2f0c2eda876e2cc2
Gaticus Hax <gaticus.hax@gmail.com>
2019-08-17 11:47:17 -0700

    refactor to use lib/validate_asset.bat

--- lib/validate_asset.bat
+++ lib/validate_asset.bat
@@ -2,22 +2,27 @@
 REM returns ERRORLEVEL 0 if passed
 REM returns ERRORLEVEL 1 if failed
 
-if "%~x1"==".DDS"      exit /b 0
-if "%~x1"==".MBIN"     exit /b 0
-if "%~x1"==".BIN"      exit /b 0
-if "%~x1"==".H"        exit /b 0
-if "%~x1"==".GLSL"     exit /b 0
-if "%~x1"==".PC"       exit /b 0
-if "%~x1"==".SQS"      exit /b 0
-if "%~x1"==".TXT"      exit /b 0
-if "%~x1"==".CSV"      exit /b 0
-if "%~x1"==".WEM"      exit /b 0
-if "%~x1"==".BNK"      exit /b 0
-if "%~x1"==".XML"      exit /b 0
-if "%~x1"==".TTC"      exit /b 0
-if "%~x1"==".TTF"      exit /b 0
-if "%~x1"==".FNT"      exit /b 0
-if "%~x1"==".JSON"     exit /b 0
-
-REM not a valid asset type
+set VALID_TYPES=DDS MBIN BIN H GLSL PC SQS TXT CSV WEM BNK XML TTC TTF FNT JSON
+for %%I in (%VALID_TYPES%) do if /i "%~x1"==".%%I" exit /b 0
+REM invalid asset type
 exit /b 1
+
+REM if /i "%~x1"==".DDS"      exit /b 0
+REM if /i "%~x1"==".MBIN"     exit /b 0
+REM if /i "%~x1"==".BIN"      exit /b 0
+REM if /i "%~x1"==".H"        exit /b 0
+REM if /i "%~x1"==".GLSL"     exit /b 0
+REM if /i "%~x1"==".PC"       exit /b 0
+REM if /i "%~x1"==".SQS"      exit /b 0
+REM if /i "%~x1"==".TXT"      exit /b 0
+REM if /i "%~x1"==".CSV"      exit /b 0
+REM if /i "%~x1"==".WEM"      exit /b 0
+REM if /i "%~x1"==".BNK"      exit /b 0
+REM if /i "%~x1"==".XML"      exit /b 0
+REM if /i "%~x1"==".TTC"      exit /b 0
+REM if /i "%~x1"==".TTF"      exit /b 0
+REM if /i "%~x1"==".FNT"      exit /b 0
+REM if /i "%~x1"==".JSON"     exit /b 0
+
+REM REM not a valid asset type
+REM exit /b 1
